8d6e0d14061a8bc05e87533e5614e62a393eeda1
Joel Valencia <trekkerj1@gmail.com>
2016-03-14 14:50:41 -0500

    mapAsync

--- UsePromises/usePromiseTemplate.js
+++ UsePromises/usePromiseTemplate.js
@@ -25,28 +25,6 @@ function mapAsync(iterator, obj, context) {
 };
 function mapAsyncInOrder(iterator, array, context) {
     
-    var promise = new Promise(function(resolve, reject) {
-        var result = []
-        
-        function next(i) {
-            //console.log(i);
-            if (result.length < array.length) {
-                iterator(array[i]).then(function(arg) {
-                    result.push(arg);
-                    i++;
-                    next(i);
-                })
-            } else {
-                console.log(result);
-                resolve(result);
-            }
-        }
-        
-        var i = 0;
-        next(i);
-    })
-    
-    return promise;
 };
 function mapAsyncInDescendingOrder(iterator, array, context) {
     
