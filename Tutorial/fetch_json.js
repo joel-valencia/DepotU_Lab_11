17e9a3d348be0adb4dc0a6f946b144f969e417f1
Joel Valencia <trekkerj1@gmail.com>
2016-03-14 14:49:58 -0500

    mapAsync

---
+++ Tutorial/fetch_json.js
@@ -0,0 +1,22 @@
+// $.ajax({
+//     url: 'http://localhost:1337/',
+//     method: 'GET'
+// }).then(function(result) {
+//     console.log(result);
+// })
+
+// var xhttp = new XMLHttpRequest();
+
+// xhttp.onreadystatechange = function() {
+//     if (xhttp.readyState == 4 && xhttp.status == 200) {
+//         console.log(xhttp.responseText);
+//     }
+// };
+// xhttp.open("GET", "http://localhost:1337/", true);
+// xhttp.send();
+
+// http.get('http://localhost:1337/', (res) => {
+//   console.log(res);
+//   // consume response body
+//   //res.resume();
+// })
--- UsePromises/usePromiseTemplate.js
+++ UsePromises/usePromiseTemplate.js
@@ -1,3 +1,53 @@
-function mapAsync(iterator, obj, context) {};
-function mapAsyncInOrder(iterator, array, context) {};
-function mapAsyncInDescendingOrder(iterator, array, context) {};
+function mapAsync(iterator, obj, context) {
+    // console.log(iterator);
+    // console.log(obj);
+    // console.log(context);
+    
+    var promise = new Promise(function(resolve, reject) {
+        var result = [];
+            
+        for (var i in obj) {
+            //console.log(i);
+            iterator(obj[i]).then(function(arg) {
+                //console.log(i);
+                result.push(arg);
+                // console.log(result);
+                if (result.length == obj.length) {
+                    //console.log(result);
+                    resolve(result);
+                }
+            })
+        }
+    })
+
+    return promise;
+    
+};
+function mapAsyncInOrder(iterator, array, context) {
+    
+    var promise = new Promise(function(resolve, reject) {
+        var result = []
+        
+        function next(i) {
+            //console.log(i);
+            if (result.length < array.length) {
+                iterator(array[i]).then(function(arg) {
+                    result.push(arg);
+                    i++;
+                    next(i);
+                })
+            } else {
+                console.log(result);
+                resolve(result);
+            }
+        }
+        
+        var i = 0;
+        next(i);
+    })
+    
+    return promise;
+};
+function mapAsyncInDescendingOrder(iterator, array, context) {
+    
+};
