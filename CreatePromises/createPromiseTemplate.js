dc02e21afbda98d5f5cc42ac4fa9dfbff872dbc0
Joel Valencia <trekkerj1@gmail.com>
2016-03-14 15:28:00 -0500

    reject

--- CreatePromises/createPromiseTemplate.js
+++ CreatePromises/createPromiseTemplate.js
@@ -1,24 +1,34 @@
 var APromise = {};
 
 APromise.all = function(promises){
-    console.log(promises);
+    //console.log(promises);
     
 };
 
 APromise.race = function(promises){
-    
+    //console.log(promises);
 };
 
 APromise.resolve = function(value){
+    console.log(value);
+    
     var promise = new Promise(function(resolve, reject) {
         resolve(value);
     })
     
+    //promise.resolve(value);
+    //console.log(promise);
     return promise;
 };
 
 APromise.reject = function(err){
+    //console.log(err);
+    var promise = new Promise(function(resolve, reject) {
+        reject(err);
+    })
     
+    //promise.reject(err);
+    return promise;
 };
 
 //module.exports.APromise = APromise;
