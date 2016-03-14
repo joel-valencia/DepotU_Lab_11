4c484368af0e72cdbc42d726cc9a299e40caa80e
Joel Valencia <trekkerj1@gmail.com>
2016-03-14 15:53:05 -0500

    race

--- CreatePromises/createPromiseTemplate.js
+++ CreatePromises/createPromiseTemplate.js
@@ -7,10 +7,22 @@ APromise.all = function(promises){
 
 APromise.race = function(promises){
     //console.log(promises);
+    
+    var promise = new Promise(function(resolve, reject) {
+        for (var i in promises) {
+            promises[i].then(function(value) {
+                resolve(value);
+            }, function(reason) {
+                reject(reason);
+            });
+        }
+    })
+    
+    return promise;
 };
 
 APromise.resolve = function(value){
-    console.log(value);
+    //console.log(value);
     
     var promise = new Promise(function(resolve, reject) {
         resolve(value);
